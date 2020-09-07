52706ccbd81130be8ea693b5ca1930ac3755d3a2
Alie Stone <helloaliestone@gmail.com>
2020-09-07 19:42:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 def countdown(seconds, with_sleep)
   while seconds > 0
     puts "#{seconds.to_s} SECOND(S)!"
-    seconds -= 1 
-    if with_sleep 
-      sleep 1 
-    end 
+    seconds -= 1
+    if with_sleep
+      sleep 1
+    end
   end
   return "HAPPY NEW YEAR!"
 end
@@ -12,4 +12,3 @@ end
  def countdown_with_sleep(seconds)
    countdown(seconds, true)
   end
-
