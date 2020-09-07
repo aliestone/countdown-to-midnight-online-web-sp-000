9f99de1b172a9398d622bf939b35b915c1f1e108
Alie Stone <helloaliestone@gmail.com>
2020-09-07 19:42:18 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,16 +1,15 @@
-def countdown (seconds)
+def countdown(seconds, with_sleep)
   while seconds > 0
     puts "#{seconds.to_s} SECOND(S)!"
-    seconds -= 1
+    seconds -= 1 
+    if with_sleep 
+      sleep 1 
+    end 
   end
   return "HAPPY NEW YEAR!"
 end
 
- def countdown_with_sleep (seconds)
-   while seconds > 0
-     puts "#{seconds.to_s} SECOND(S)!"
-     seconds -= 1
-     sleep 1
-   end
-   return "HAPPY NEW YEAR"
- end
+ def countdown_with_sleep(seconds)
+   countdown(seconds, true)
+  end
+
